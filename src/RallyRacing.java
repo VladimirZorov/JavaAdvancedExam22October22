0c0a68e1758e4664a0d5c416d95dfd104f8752cc
Vladimir Zorov <vl.zorov@gmail.com>
2022-11-25 19:31:07 +0200

    Add

--- src/RallyRacing.java
+++ src/RallyRacing.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class RallyRacing {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = Integer.parseInt(scanner.nextLine());
+        String racingNumber = scanner.nextLine();
+
+        char[][] raceRoute = new char[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String line = scanner.nextLine();
+            raceRoute[i] = line.toCharArray();
+        }
+
+
+        System.out.println();
+    }
 }
